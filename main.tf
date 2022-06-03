3a62df1bdff7aa0786aa1559aa5a617158d66a21
tsu019 <marisachanth@gmail.com>
2022-06-03 12:24:14 -0700

    Update main.tf
    remove distribution tag

--- main.tf
+++ main.tf
@@ -21,9 +21,4 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "ubuntu" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
-
-  tags = {
-    Name                 = var.instance_name
-    "Linux Distribution" = "Ubuntu"
-  }
 }
